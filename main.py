035db8bd474a2528ac51db92d5e9adc8cdf62668
Elijah Wang <elijah.wang.yyz9087@gmail.com>
2023-12-10 21:39:33 -0800

    Add dependencies

--- main.py
+++ main.py
@@ -56,6 +56,7 @@ def bruteForceCheck(password_len=4):
     elif checkMode("BCrypt"):
         salt = bcrypt.gensalt()
         check = bcrypt.hashpw(l, salt) 
+        print(check)
     if(check == pw):
 #            t.terminate()
             print("--- Password is :",line.rstrip(),"---")
@@ -77,34 +78,31 @@ if pw in ['-m','-s', '-b', '-d', '-c']:
     quit()
 
 
-try:
-    # Parsing argument
-    arguments, values = getopt.getopt(argumentList, options)
-     
-    # checking each argument
-    for currentArgument, currentValue in arguments:
+# Parsing argument
+arguments, values = getopt.getopt(argumentList, options)
  
-             
-        if currentArgument in ("-m"):
-            setMode("MD5")
-             
-        elif currentArgument in ("-s"):
-            setMode("SHA-256")
+# checking each argument
+for currentArgument, currentValue in arguments:
+ 
+         
+    if currentArgument in ("-m"):
+        setMode("MD5")
+         
+    elif currentArgument in ("-s"):
+        setMode("SHA-256")
+    
+    elif currentArgument in ("-c"):
+        setMode("BCrypt")
         
-        elif currentArgument in ("-c"):
-            setMode("BCrypt")
-            
-        elif currentArgument in ("-b"):
-            setAttack("Brute Force", "")
-            bruteForceCheck()
-            quit()
-        
-        elif currentArgument in ("-d"):
-            #print ("Dictionary file Name: ", sys.argv[sys.argv.index("-d")+1])  
-            setAttack("Dictionary", sys.argv[sys.argv.index("-d")+1])
-except Exception as e:
-    sys.exit(e)
-        
+    elif currentArgument in ("-b"):
+        setAttack("Brute Force", "")
+        bruteForceCheck()
+        quit()
+    
+    elif currentArgument in ("-d"):
+        #print ("Dictionary file Name: ", sys.argv[sys.argv.index("-d")+1])  
+        setAttack("Dictionary", sys.argv[sys.argv.index("-d")+1])
+       
 count = 0
 
 start_time = time.time()
